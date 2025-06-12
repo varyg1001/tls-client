af7cfe7bf496b6616ce2d0608756afcd1ab8c573
varyg <88599103+varyg1001@users.noreply.github.com>
2025-06-12 17:48:59 +0200

    Test

--- tls_client/update_shared_libraries.py
+++ tls_client/update_shared_libraries.py
@@ -1,4 +1,5 @@
 import requests
+import platform
 
 shared_library_version = "1.9.1"
 github_download_url = "https://github.com//bogdanfinn/tls-client/releases/download/v{}/{}"
@@ -26,6 +27,17 @@ dependency_filenames = [
     "tls-client-x86.so",
     "tls-client-arm64.so"
 ]
+
+system = platform.system()
+if system == "Windows":
+    github_repo_filenames = github_repo_filenames[:2]
+    dependency_filenames = dependency_filenames[:2]
+elif system == "Darwin":
+    github_repo_filenames = github_repo_filenames[2:4]
+    dependency_filenames = dependency_filenames[2:4]
+elif system == "Linux":
+    github_repo_filenames = github_repo_filenames[4:]
+    dependency_filenames = dependency_filenames[4:]
 
 for github_filename, dependency_filename in zip(github_repo_filenames, dependency_filenames):
     response = requests.get(
